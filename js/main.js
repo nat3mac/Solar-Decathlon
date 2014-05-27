3343dd1ead51d56f5724e83cac84f4fc169a2440
ch0c0lat <chin.eric.s@gmail.com>
2014-05-26 21:23:26 -0700

    language toggle buttons
    they work visually, and set a variable that we can use to read state to
    change the text

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
-console.log('ready');
+	console.log('ready');
 
+	var langSet='en';
+	
+	$('#englishIcon').click(
+		function(){
+			if(langSet=='es'){
+				$('#englishIcon').css('opacity', '1');
+				$('#spanishIcon').css('opacity', '.3');
+				langSet='en';
+	
+			}
+		}
+	);
+	
+	$('#spanishIcon').click(
+		function(){
+			if(langSet=='en'){
+				$('#englishIcon').css('opacity', '.3');
+				$('#spanishIcon').css('opacity', '1');
+				langSet='es';
+			}
+		}
+	);
+	
 });
