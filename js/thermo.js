49a64e9a6cae3922566eb13d8989a28d5339729a
nat3mac <natemac1@scc2103-13.ucdavis.edu>
2014-05-27 16:05:35 -0700

    maths

--- js/thermo.js
+++ js/thermo.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
 
+var tempsetting;
+var tempdif;
+var temptop;
+
 $('#slider').draggable({
         containment:'parent',
         axis:'y',
         drag:function(e,ui){
-
+			tempsetting = $('#slider').position();
+			temptop = tempsetting.top;
+			tempdif = temptop/24;
+			console.log (tempdif);
+			$('#temp').html(30-tempdif);
         }
     });
+    
+    
 
 });
