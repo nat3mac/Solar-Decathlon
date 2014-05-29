9ad1126fc77c4ac9a3814aa2d0511552342d6399
ch0c0lat <chin.eric.s@gmail.com>
2014-05-29 04:16:57 -0700

    set up structures, done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,39 @@ $(document).ready(function(){
 
 	var langSet='en';
 	
+	$('#selectThermo').click(
+		function(){
+			$('#selectThermo').fadeOut(0);
+			$('#selectFeedback').fadeIn();
+			$('#selectLights').fadeIn();
+			$('#thermostat').fadeIn();
+			$('#energyUse').fadeOut(0);
+			$('#roomSelect').fadeOut(0);
+		}
+	);
+	
+	$('#selectLights').click(
+		function(){
+			$('#selectLights').fadeOut(0);
+			$('#selectFeedback').fadeIn();
+			$('#selectThermo').fadeIn();
+			$('#roomSelect').fadeIn();
+			$('#energyUse').fadeOut(0);
+			$('#thermostat').fadeOut(0);
+		}
+	);
+	
+	$('#selectFeedback').click(
+		function(){
+			$('#selectFeedback').fadeOut(0);
+			$('#selectThermo').fadeIn();
+			$('#selectLights').fadeIn();
+			$('#energyUse').fadeIn();
+			$('#roomSelect').fadeOut(0);
+			$('#thermostat').fadeOut(0);
+		}
+	);
+	
 	$('#englishIcon').click(
 		function(){
 			if(langSet=='es'){
