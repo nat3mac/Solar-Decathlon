36ed22e186df3437a7e2ffdfd53f9e1a6ef009ac
Nate MacMillan <nate@isotix.com>
2014-05-29 07:00:38 -0700

    language

--- js/language.js
+++ js/language.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 
 	$('#englishIcon').click(
 		function(){
+			$('.en').fadeIn();
+			$('.es').fadeOut(0);
+
 			if(langSet=='es'){
 				$('#englishIcon').css('opacity', '1');
 				$('#spanishIcon').css('opacity', '.3');
@@ -13,6 +16,9 @@ $(document).ready(function(){
 	
 	$('#spanishIcon').click(
 		function(){
+			$('.en').fadeOut(0);
+			$('.es').fadeIn();
+
 			if(langSet=='en'){
 				$('#englishIcon').css('opacity', '.3');
 				$('#spanishIcon').css('opacity', '1');
