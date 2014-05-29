915d34abcae98af3eb40450183733a7e518323f5
Nate MacMillan <nate@isotix.com>
2014-05-29 06:11:55 -0700

    change icons

---
+++ js/language.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	$('#englishIcon').click(
+		function(){
+			if(langSet=='es'){
+				$('#englishIcon').css('opacity', '1');
+				$('#spanishIcon').css('opacity', '.3');
+				langSet='en';
+	
+			}
+		}
+	);
+	
+	$('#spanishIcon').click(
+		function(){
+			if(langSet=='en'){
+				$('#englishIcon').css('opacity', '.3');
+				$('#spanishIcon').css('opacity', '1');
+				langSet='es';
+			}
+		}
+	);
+});
