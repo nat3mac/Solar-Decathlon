344c1a191d6502cca5c066c593a37901f4005a4c
nat3mac <natemac1@scc2103-24.ucdavis.edu>
2014-06-03 16:10:39 -0700

    temp units

--- js/thermo.js
+++ js/thermo.js
@@ -12,11 +12,27 @@ $('#slider').draggable({
 			tempsetting = $('#slider').position();
 			temptop = tempsetting.top;
 			tempdif = temptop/24;
+			
+			if (tempUnit=='cel'){
 			$('#temp').html(Math.round(30-tempdif));
+			$('.far').fadeOut(0);
+			$('.cel').fadeIn(0);
+			}
+			else if (tempUnit=='far'){
+			$('#temp').html(Math.round(((30-tempdif)*1.8)+32));
+			$('.far').fadeIn(0);
+			$('.cel').fadeOut(0);
+			}
         }
     });
+
+	function setFar(){
+			tempUnit='far';
+			};
+	function setCel(){
+			tempUnit='cel';
+			};
+	$('#configFar').click(setFar);
+	$('#configCel').click(setCel);
 });
 
-/*if (tempUnit=='far'){
-	$('#temp').html(Math.round(((30-tempdif)*1.8)+32))
-}*/
