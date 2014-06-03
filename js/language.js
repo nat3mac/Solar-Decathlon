653bc5dd0e7228edaef5f85ca38a86b62e904fef
nat3mac <natemac1@scc2103-24.ucdavis.edu>
2014-06-03 15:47:16 -0700

    language fix

--- js/language.js
+++ js/language.js
@@ -1,35 +1,43 @@
 $(document).ready(function(){
 
 	var langSet='es';
-
-	$('#englishIcon').click(
-		function(setEn){
+	function setEn(){
 			$('.en').fadeIn(0);
 			$('.es').fadeOut(0);
+			langSet='en';
+			};
+	function setEs(){
+			$('.en').fadeOut(0);
+			$('.es').fadeIn(0);
+			langSet='es';
+			};
 
+	$('#englishIcon').click(
+		function(){
+			setEn();
 			if(langSet=='es'){
 				$('#englishIcon').css('opacity', '1');
 				$('#spanishIcon').css('opacity', '.3');
-				langSet='en';
+				
 	
 			}
 		}
 	);
 	
 	$('#spanishIcon').click(
-		function(setEs){
-			$('.en').fadeOut(0);
-			$('.es').fadeIn(0);
+		function(){
+			setEs();
 
 			if(langSet=='en'){
 				$('#englishIcon').css('opacity', '.3');
 				$('#spanishIcon').css('opacity', '1');
-				langSet='es';
+				
 			}
 		}
 	);
 
-//	$('.english').click(setEn);
-//	$('.spanish').click(setEs);
+	$('#configEn').click(setEn);
+		
+	$('#configEs').click(setEs);
 
 });
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
 	console.log('ready');
-
-
 	
 	$('#selectThermo').click(
 		function(){
