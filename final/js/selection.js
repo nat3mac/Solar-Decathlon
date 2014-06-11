00cea38e5157b6265f7f1fa24235700e12d4e836
Nate MacMillan <nate@isotix.com>
2014-06-11 06:33:01 -0700

    gradients

--- final/js/selection.js
+++ final/js/selection.js
@@ -8,25 +8,25 @@ $(document).ready(function(){
 		$('#thermostat').fadeIn(200);
 		$('#lights').fadeOut(0);
 		$('#feedback').fadeOut(0);
-		$('#selectThermo').css('backgroundColor', '#303030')
-		$('#selectLights').css('backgroundColor', '#404040')
-		$('#selectFeedback').css('backgroundColor', '#404040')
+		$('#selectThermo').css('background', '#303030')
+		$('#selectLights').css('background', 'linear-gradient(#404040, #545454)')
+		$('#selectFeedback').css('background', 'linear-gradient(#404040, #545454)')
 	});
 	$('#selectLights').click(function(){
 		$('#lights').fadeIn(200);
 		$('#thermostat').fadeOut(0);
 		$('#feedback').fadeOut(0);
-		$('#selectLights').css('backgroundColor', '#303030')
-		$('#selectThermo').css('backgroundColor', '#404040')
-		$('#selectFeedback').css('backgroundColor', '#404040')
+		$('#selectLights').css('background', '#303030')
+		$('#selectThermo').css('background', 'linear-gradient(#404040, #545454)')
+		$('#selectFeedback').css('background', 'linear-gradient(#404040, #545454)')
 	});
 	$('#selectFeedback').click(function(){
 		$('#feedback').fadeIn(200);
 		$('#lights').fadeOut(0);
 		$('#thermostat').fadeOut(0);
-		$('#selectFeedback').css('backgroundColor', '#303030')
-		$('#selectLights').css('backgroundColor', '#404040')
-		$('#selectThermo').css('backgroundColor', '#404040')
+		$('#selectFeedback').css('background', '#303030')
+		$('#selectLights').css('background', 'linear-gradient(#404040, #545454)')
+		$('#selectThermo').css('background', 'linear-gradient(#404040, #545454)')
 	});
 
 }); //close document ready
