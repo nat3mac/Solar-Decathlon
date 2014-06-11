a9d4a5e5522a90622a30e0ff59ca8f12088bc42f
ch0c0lat <chin.eric.s@gmail.com>
2014-06-11 05:02:35 -0700

    bg color changes

--- final/js/selection.js
+++ final/js/selection.js
@@ -9,24 +9,24 @@ $(document).ready(function(){
 		$('#lights').fadeOut(0);
 		$('#feedback').fadeOut(0);
 		$('#selectThermo').css('backgroundColor', '#303030')
-		$('#selectLights').css('backgroundColor', '#4d4d4d')
-		$('#selectFeedback').css('backgroundColor', '#4d4d4d')
+		$('#selectLights').css('backgroundColor', '#404040')
+		$('#selectFeedback').css('backgroundColor', '#404040')
 	});
 	$('#selectLights').click(function(){
 		$('#lights').fadeIn(200);
 		$('#thermostat').fadeOut(0);
 		$('#feedback').fadeOut(0);
 		$('#selectLights').css('backgroundColor', '#303030')
-		$('#selectThermo').css('backgroundColor', '#4d4d4d')
-		$('#selectFeedback').css('backgroundColor', '#4d4d4d')
+		$('#selectThermo').css('backgroundColor', '#404040')
+		$('#selectFeedback').css('backgroundColor', '#404040')
 	});
 	$('#selectFeedback').click(function(){
 		$('#feedback').fadeIn(200);
 		$('#lights').fadeOut(0);
 		$('#thermostat').fadeOut(0);
 		$('#selectFeedback').css('backgroundColor', '#303030')
-		$('#selectLights').css('backgroundColor', '#4d4d4d')
-		$('#selectThermo').css('backgroundColor', '#4d4d4d')
+		$('#selectLights').css('backgroundColor', '#404040')
+		$('#selectThermo').css('backgroundColor', '#404040')
 	});
 
 }); //close document ready
