59179a7f3f329b7b08d14fea1092d8dc69acfe57
ch0c0lat <chin.eric.s@gmail.com>
2014-06-11 03:17:39 -0700

    Slider Rough

--- final/js/thermostat.js
+++ final/js/thermostat.js
@@ -2,5 +2,14 @@ $(document).ready(function(){
 
 	//console initialization
 	console.log('thermostat js initialized');
+	
+	$('#sliderElement').slider(
+		{animate: true}, 
+		{orientation: 'vertical'}, 
+		{min:0}, 
+		{max: 100}, 
+		{step: 1}, 
+		{value: 50}
+	);
 
 }); //close document ready
