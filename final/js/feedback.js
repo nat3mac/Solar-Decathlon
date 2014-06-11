ac91b10fdd0d51752462b68b0c2b76da98f73c63
Nate <nate@isotix.com>
2014-06-11 02:56:31 -0700

    percentage

--- final/js/feedback.js
+++ final/js/feedback.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
 	//console initialization
 	console.log('feedback js initialized');
 
+	var percentage = 64;
+	$('#percentage').html(percentage + '%');
+
 }); //close document ready
