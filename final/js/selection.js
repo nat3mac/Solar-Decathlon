73711302d654a23abbbf051e97be862e78586871
ch0c0lat <chin.eric.s@gmail.com>
2014-06-10 20:13:11 -0700

    Working Selection
    the selection system now works for content

--- final/js/selection.js
+++ final/js/selection.js
@@ -1,6 +1,35 @@
 $(document).ready(function(){
 
 	//console initialization
-	console.log('lights js initialized');
+	console.log('selection js initialized');
+		
+	//click
+	$('#selectThermo').click(function(){
+		selectedState = 'thermo';
+		$('#thermostat').fadeIn(200);
+		$('#lights').fadeOut(0);
+		$('#feedback').fadeOut(0);
+		$('#selectThermo').css('backgroundColor', '#303030')
+		$('#selectLights').css('backgroundColor', '#4d4d4d')
+		$('#selectFeedback').css('backgroundColor', '#4d4d4d')
+	});
+	$('#selectLights').click(function(){
+		selectedState = 'lights';
+		$('#lights').fadeIn(200);
+		$('#thermostat').fadeOut(0);
+		$('#feedback').fadeOut(0);
+		$('#selectLights').css('backgroundColor', '#303030')
+		$('#selectThermo').css('backgroundColor', '#4d4d4d')
+		$('#selectFeedback').css('backgroundColor', '#4d4d4d')
+	});
+	$('#selectFeedback').click(function(){
+		selectedState = 'feedback';
+		$('#feedback').fadeIn(200);
+		$('#lights').fadeOut(0);
+		$('#thermo').fadeOut(0);
+		$('#selectFeedback').css('backgroundColor', '#303030')
+		$('#selectLights').css('backgroundColor', '#4d4d4d')
+		$('#selectThermo').css('backgroundColor', '#4d4d4d')
+	});
 
 }); //close document ready
