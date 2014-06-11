7beceb6856a73b7f1a90525b708d6ffa0272d2f4
Nate MacMillan <nate@isotix.com>
2014-06-11 06:58:26 -0700

    gradients again

--- final/js/selection.js
+++ final/js/selection.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
 		$('#thermostat').fadeIn(200);
 		$('#lights').fadeOut(0);
 		$('#feedback').fadeOut(0);
-		$('#selectThermo').css('backgroundColor', '#303030');
-		$('#selectLights').css('backgroundColor', '#404040');
-		$('#selectFeedback').css('backgroundColor', '#404040');
+		$('#selectThermo').css('background', '#303030');
+		$('#selectLights').css('background', 'linear-gradient(#404040, #545454)');
+		$('#selectFeedback').css('background', 'linear-gradient(#404040, #545454)');
 		$('#thermoBlock').fadeIn(200);
 		$('#lightsBlock').fadeOut(200);
 		$('#feedbackBlock').fadeOut(200);
@@ -19,9 +19,9 @@ $(document).ready(function(){
 		$('#lights').fadeIn(200);
 		$('#thermostat').fadeOut(0);
 		$('#feedback').fadeOut(0);
-		$('#selectLights').css('backgroundColor', '#303030');
-		$('#selectThermo').css('backgroundColor', '#404040');
-		$('#selectFeedback').css('backgroundColor', '#404040');
+		$('#selectLights').css('background', '#303030');
+		$('#selectThermo').css('background', 'linear-gradient(#404040, #545454)');
+		$('#selectFeedback').css('background', 'linear-gradient(#404040, #545454)');
 		$('#lightsBlock').fadeIn(200);
 		$('#thermoBlock').fadeOut(200);
 		$('#feedbackBlock').fadeOut(200);
@@ -30,9 +30,9 @@ $(document).ready(function(){
 		$('#feedback').fadeIn(200);
 		$('#lights').fadeOut(0);
 		$('#thermostat').fadeOut(0);
-		$('#selectFeedback').css('backgroundColor', '#303030');
-		$('#selectLights').css('backgroundColor', '#404040');
-		$('#selectThermo').css('backgroundColor', '#404040');
+		$('#selectFeedback').css('background', '#303030');
+		$('#selectLights').css('background', 'linear-gradient(#404040, #545454)');
+		$('#selectThermo').css('background', 'linear-gradient(#404040, #545454)');
 		$('#feedbackBlock').fadeIn(200);
 		$('#lightsBlock').fadeOut(200);
 		$('#thermoBlock').fadeOut(200);
